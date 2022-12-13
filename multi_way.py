f30a2e765ad98ab7e370d3ae71590418c627e4cc
ironMan <oleg.plugaru.md@gmail.com>
2022-12-13 22:12:31 +0200

    multi way example

---
+++ multi_way.py
@@ -0,0 +1,46 @@
+x = 6
+if x < 2 :
+    print('small')
+elif x < 10 :
+    print('Medium')
+else :
+    print('LARGE')
+print('All done')
+
+# No else
+x = 2
+if x < 2 :
+    print('Small')
+elif x < 10 :
+    print('Medium')
+print('All done')
+
+if x < 2 :
+    print('Small')
+elif x < 10 :
+    print('Medium')
+elif x < 20 :
+    print('Big')
+elif x < 40 :
+    print('Large')
+elif x < 100:
+    print('Huge')
+else :
+    print('Ginormous')
+
+# Wrong code
+# if x < 2 :
+#     print('Below 2')
+# elif x >= 2 :
+#     print('Two or more')
+# else : print('Something else')
+#
+# if x < 2 :
+#     print('Below 2')
+# elif x < 20 :
+#     print('Below 20')
+# elif x < 10 :
+#     print('Below 10')
+# else :
+#     print('Something else')
+
