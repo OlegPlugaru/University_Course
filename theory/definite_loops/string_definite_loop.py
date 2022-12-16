7f911684ea94d8654c48be7496bd81114213b531
ironMan <oleg.plugaru.md@gmail.com>
2022-12-16 22:05:55 +0200

    adde one for loop in this file

--- theory/definite_loops/string_definite_loop.py
+++ theory/definite_loops/string_definite_loop.py
@@ -2,3 +2,8 @@ friends = ['Joseph', 'Glenn', 'Sally']
 for friend in friends :
     print('Happy New Year:', friend)
 print('Done!')
+
+print('\nBefore')
+for thing in [9, 41, 12, 3, 74, 15] :
+    print(thing)
+print('After')
