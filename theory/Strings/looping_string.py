aa30d5f26a880761e2878f166c40eaa429c9231c
ironMan <oleg.plugaru.md@gmail.com>
2022-12-18 14:49:37 +0200

    added few programs

--- theory/Strings/looping_string.py
+++ theory/Strings/looping_string.py
@@ -4,3 +4,17 @@ while index < len(fruit) :
     letter = fruit[index]
     print(index, letter)
     index += 1
+
+print('\n\n')
+
+for letter in fruit :
+    print(letter)
+
+print('\n\n')
+
+index = 0
+for letter in fruit :
+    print(index, letter)
+    index += 1
+
+
