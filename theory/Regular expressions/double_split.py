f5cc3ca48cc73309e8bb6f5cd33bb1c69124ea3b
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-01-08 17:14:47 +0200

    added a program who find hosts names  in text file.

---
+++ theory/Regular expressions/double_split.py
@@ -0,0 +1,12 @@
+import re
+
+data = open('mbox.txt')
+
+for line in data:
+    line = line.rstrip()
+    if  line.startswith("From") :
+
+        words = line.split()
+        email = words[1]
+        pieces = email.split('@')
+        print(pieces[1])
