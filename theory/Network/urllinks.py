eb692528c9e512f2b7bc900ba84c1a73d21ef897
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-01-09 21:20:30 +0200

    added a program who show all the url link wrom the wep page

---
+++ theory/Network/urllinks.py
@@ -0,0 +1,29 @@
+# To run this, you can install BeautifullSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+if len(url) < 1:
+    html = urllib.request.urlopen("https://wiki.archlinux.org/title/python", context=ctx).read()
+else:
+    html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href',None))
