d5ecc9ce55ea82d233293677311cb8b9e17e1632
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-01-09 19:38:47 +0200

    added a program who reading a web page

--- theory/Network/reading_web.py
+++ theory/Network/reading_web.py
@@ -1,7 +1,7 @@
 import urllib.request
-# import urllib.parse
+import urllib.parse
 import urllib.error
 
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
 for line in fhand:
     print(line.decode().strip())
