ebe174fab0ec32060f918c754e03a44984f09031
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-01-11 13:52:29 +0200

    added a program that works with google maps API

---
+++ theory/Web_Services/geojson.py
@@ -0,0 +1,36 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
+
+# Note that Google is increasingly requiring keys
+# for this API
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+while True:
+    adress = input('Enter location: ')
+    if len(adress) < 1:
+        break
+
+    url = serviceurl + urllib.parse.urlencode({'address': adress})
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status'not in js or js['status'] != 'OK':
+        print('=== Failure to retrieve===')
+        print(data)
+        continue
+
+    lat = js["results"][0]["geometry"]["location"]["lat"]
+    lng = js["results"][0]["geometry"]["location"]["lng"]
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formated_adress']
+    print(location)
